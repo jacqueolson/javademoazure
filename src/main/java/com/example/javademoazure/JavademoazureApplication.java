c1e89380ef9cbba4da985cf656ea3e07c6763e41
Jacque Olson <jacque.w.olson@gmail.com>
2018-06-04 21:51:40 -0500

    init

---
+++ src/main/java/com/example/javademoazure/JavademoazureApplication.java
@@ -0,0 +1,12 @@
+package com.example.javademoazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavademoazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavademoazureApplication.class, args);
+	}
+}
